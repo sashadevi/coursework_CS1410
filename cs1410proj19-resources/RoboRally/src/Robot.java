55d6d02abea03ca7868e3f4fb3040e67325c1efc
sashadevi <sashadevi@me.com>
2019-04-18 10:51:18 +0100

    Update Robot.java

--- cs1410proj19-resources/RoboRally/src/Robot.java
+++ cs1410proj19-resources/RoboRally/src/Robot.java
@@ -15,16 +15,122 @@ public class Robot extends Piece{
 	public void addAction(String action) {
 		actions.add(action);
 	}
-	public void goForward(){}
-	public void goBackward(){}
-	public void rotateLeft(){}
-	public void rotateRight(){}
-	public void makeUturn(){}
-	public void makeWait(){}
+	public void goForward(){
+		int row = super.getLocation().getRow();
+		int col = super.getLocation().getCol();
+		switch(d) {
+			case NORTH:
+				row = row + 1;
+				super.setLocation(row, col);
+				break;
+
+			case SOUTH:
+				row = row - 1;
+				super.setLocation(row, col);
+				break;
+
+			case EAST:
+				col = col + 1;
+				super.setLocation(row, col);
+				break;
+
+			case WEST:
+				col = col - 1;
+				super.setLocation(row, col);
+				break;
+		}
+	}
+	public void goBackward(){
+		int row = super.getLocation().getRow();
+		int col = super.getLocation().getCol();
+		switch(d) {
+			case NORTH:
+				row = row - 1;
+				super.setLocation(row, col);
+				break;
+
+			case SOUTH:
+				row = row + 1;
+				super.setLocation(row, col);
+				break;
+
+			case EAST:
+				col = col - 1;
+				super.setLocation(row, col);
+				break;
+
+			case WEST:
+				col = col + 1;
+				super.setLocation(row, col);
+				break;
+		}
+	}
+	public void rotateLeft(){
+		switch(d) {
+			case NORTH:
+				d = NORTH.turnCounterClockwise();
+				break;
+
+			case EAST:
+				d = EAST.turnCounterClockwise();
+				break;
+
+			case SOUTH:
+				d = SOUTH.turnCounterClockwise();
+				break;
+
+			case WEST:
+				d = WEST.turnCounterClockwise();
+				break;
+		}
+	}
+	public void rotateRight(){
+		switch(d) {
+			case NORTH:
+				d = NORTH.turnClockwise();
+				break;
+
+			case EAST:
+				d = EAST.turnClockwise();
+				break;
+
+			case SOUTH:
+				d = SOUTH.turnClockwise();
+				break;
+
+			case WEST:
+				d = WEST.turnClockwise();
+				break;
+		}
+	}
+	public void makeUturn(){
+		switch (d) {
+			case NORTH:
+				d = NORTH.makeUturn();
+				break;
+
+			case EAST:
+				d = EAST.makeUturn();
+				break;
+
+			case SOUTH:
+				d = SOUTH.makeUturn();
+				break;
+
+			case WEST:
+				d = WEST.makeUturn();
+				break;
+		}
+	}
+	public void makeWait(){
+		int row = super.getLocation().getRow();
+		int col = super.getLocation().getCol();
+		super.setLocation(row, col);
+	}
 	@Override
 	public void act(Field currentField, Field updatedField, List<Piece> newPieces) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
